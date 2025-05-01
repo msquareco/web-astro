bc350567e8dfc257b357a8e8a7e71385e1bb481c
mansuri <161573889+msquareco@users.noreply.github.com>
2025-05-01 20:44:41 +0000

    Your commit message here

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+export default {
+  integrations: [tailwind()],
+};
