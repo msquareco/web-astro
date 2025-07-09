96ad0eb9f528e561f7051b12fc70440dcf45561d
mansuri <161573889+msquareco@users.noreply.github.com>
2025-07-09 23:01:20 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-export default {
+// https://astro.build/config
+export default defineConfig({
   integrations: [tailwind()],
-};
+  output: 'static',
+});
